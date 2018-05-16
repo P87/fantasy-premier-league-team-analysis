e5144a43c7ea8a81eb9e86c2f4b7db96ae8486f1
P87 <github@peterhul.me>
2018-05-16 15:13:10 +0100

    Added graph of points per game week

--- src/main/kotlin/online/p87/fplta/model/TeamModel.kt
+++ src/main/kotlin/online/p87/fplta/model/TeamModel.kt
@@ -2,10 +2,18 @@ package online.p87.fplta.model
 
 import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
+import java.util.SortedMap
 
 class TeamModel(val name: String, val history: JsonArray<JsonObject>?) {
 
-    fun getPointsAsArray() {
+    fun getPointsAsMap(): SortedMap<Int, Int?> {
+        val pointsHashMap: HashMap<Int, Int?> = hashMapOf()
+        this.history?.let {
+            for ((week, data) in this.history.withIndex()) {
+                pointsHashMap[week] = data.int("points")
+            }
+        }
 
+        return pointsHashMap.toSortedMap()
     }
 }
--- src/main/kotlin/online/p87/fplta/view/HistoryView.kt
+++ src/main/kotlin/online/p87/fplta/view/HistoryView.kt
@@ -1,5 +1,7 @@
 package online.p87.fplta.view
 
+import javafx.scene.chart.CategoryAxis
+import javafx.scene.chart.NumberAxis
 import online.p87.fplta.model.TeamModel
 import tornadofx.*
 
@@ -8,6 +10,14 @@ class HistoryView: View("History") {
 
     override val root = vbox {
         label(teamModel.name)
+        linechart("Season History", CategoryAxis(), NumberAxis()) {
+            series(teamModel.name) {
+                for ((key, value) in teamModel.getPointsAsMap()) {
+                    data("GW ${key + 1}", value)
+                }
+
+            }
+        }
 
     }
 }
