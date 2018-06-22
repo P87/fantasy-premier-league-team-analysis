50a0eb56d2a4231b4eaa45852b95de8d2926a003
P87 <github@peterhul.me>
2018-06-22 15:59:32 +0100

    Removed unused variable

--- src/main/kotlin/online/p87/fplta/model/TeamModel.kt
+++ src/main/kotlin/online/p87/fplta/model/TeamModel.kt
@@ -7,8 +7,6 @@ import java.util.SortedMap
 class TeamModel(val name: String, val history: JsonArray<JsonObject>?) {
 
     fun getPointsAsMap(): SortedMap<Int, Int?> {
-        val pointsHashMap: HashMap<Int, Int?> = hashMapOf()
-
         val pointsMap: Map<Int, Int?> = history?.withIndex()?.map { (week, data) ->
             Pair(week, data.int("points"))
         }?.toMap() ?: emptyMap()
